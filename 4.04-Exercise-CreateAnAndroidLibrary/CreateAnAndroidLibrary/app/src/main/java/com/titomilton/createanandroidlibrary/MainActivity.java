ef6064c4cda4a528f8026c4d08eab70e86dcf7d7
Milton Siqueira <miltonsiqueira@gmail.com>
2016-09-04 18:20:29 -0400

    Solution 4.04-Exercise

---
+++ 4.04-Exercise-CreateAnAndroidLibrary/CreateAnAndroidLibrary/app/src/main/java/com/titomilton/createanandroidlibrary/MainActivity.java
@@ -0,0 +1,22 @@
+package com.titomilton.createanandroidlibrary;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.titomilton.joke.Joke;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void tellJoke(View v){
+        Intent intent = new Intent(this, Joke.class);
+        startActivity(intent);
+    }
+
+}
---
+++ 4.04-Exercise-CreateAnAndroidLibrary/CreateAnAndroidLibrary/joke/src/main/java/com/titomilton/joke/Joke.java
@@ -0,0 +1,13 @@
+package com.titomilton.joke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Joke extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke);
+    }
+}
