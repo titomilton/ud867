87c656071eb3a24332a96bb2090dc8e441b85e73
Milton Siqueira <miltonsiqueira@gmail.com>
2016-09-13 08:00:47 -0400

    Add a paid flavor

--- FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsJokeAsyncTaskAndroidTest.java
+++ FinalProject/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndpointsJokeAsyncTaskAndroidTest.java
@@ -5,6 +5,7 @@ import android.test.AndroidTestCase;
 import com.titomilton.jokelib.JokeUtils;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class EndpointsJokeAsyncTaskAndroidTest extends AndroidTestCase{
 
@@ -21,7 +22,7 @@ public class EndpointsJokeAsyncTaskAndroidTest extends AndroidTestCase{
             }
         }.execute();
 
-        signal.await();
+        signal.await(10, TimeUnit.SECONDS);
 
         assertEquals(new JokeUtils().getJoke(), joke[0]);
     }
